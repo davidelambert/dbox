d116840cd85f581f2405765dec26bd1fdb12ada4
David Lambert <lambert.david.0@gmail.com>
2019-06-23 21:48:19 -0400

    minor update to data documentation

--- R/data.R
+++ R/data.R
@@ -1,5 +1,16 @@
+#' Test Data for Distribution Visualizations
+#'
 #' A set of random distributions for visualization tests, genereated by
 #' densitybox::test_dists(). For for details see documentation there.
 #'
 #' @format A data frame of 10,000 observations and 7 variables.
+#' \describe{
+#' \item{normal}{Random normal}
+#' \item{rskew}{Right-skewed through exponentiation}
+#' \item{lskew}{Left-skewed through log transformation & exponentiation}
+#' \item{uniform}{Uniform on \[-3,3\]}
+#' \item{bimodal}{Bimodal sampled from rskew & lskew}
+#' \item{sine}{Sampled from absolute value sine function}
+#' \item{weird}{Concatenated Chi-squared distributions with random parameters}
+#' }
 "dbox_dists"
